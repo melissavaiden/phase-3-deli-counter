7e0cf76e0b8fa2cd72e6034bfd94376d5957ae05
Melissa Vaiden <melissavaiden@gmail.com>
2022-10-19 18:29:25 -0400

    commit tree 2d0f5c3bb774c78d956811734abe4268c735ba62

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+position = []
+counter = 0
+
+def take_a_number (katz_deli, name)
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+def line (katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else
+        katz_deli.map do |name|
+            
+            
+    end
+end
+
+def now_serving
+end
